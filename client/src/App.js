851c18cfed67e3f0d1d7bfc513062966160b4cf5
Rohan Paul <rohanpaul2@gmail.com>
2018-07-21 16:30:32 +0530

    explanation and comments for connect() and mapStateToProps()

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-        // <Provider store={store}>
+        <Provider store={store}>
             <div className="App">
             <AppNavbar />
             <ShoppingList />
@@ -28,3 +28,10 @@ class App extends Component {
 }
 
 export default App;
+
+/* Provider -
+https://www.sohamkamani.com/blog/2017/03/31/react-redux-connect-explained/
+
+If we want to link our React application with the redux store, we first have to let our app know that this store exists. This is where we come to the first major part of the react-redux library, which is the Provider.
+
+Provider is a React component given to us by the “react-redux” library. It serves just one purpose : to “provide” the store to its child components. */
--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 import uuid from 'uuid';
 import { getItems } from '../actions/itemActions'
 
-export default class ShoppingList extends Component {
+class ShoppingList extends Component {
 
     // for to check uuid is working, manually give some dummy data here. Later it will come from database
     // A UUID (Universal Unique Identifier) is a 128-bit number used to uniquely identify some object or entity on the Internet. Depending on the specific mechanisms used, a UUID is either guaranteed to be different or is, at least, extremely likely to be different from any other UUID generated until 3400 A.D.
@@ -66,6 +66,38 @@ export default class ShoppingList extends Component {
 }
 
 
+// When I bring in any action from redux, e.g. getItems its going to be stored as prop. And hence I am setting the PropTypes of getItems()
+ShoppingList.PropTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired
+}
+
+// what mapStateToProps() does is, it allows us to take our item state (from itemReducer.js ) and turn this into a component property so I can use it in this ShoppingList component - like e.g.
+//this.props.items
+const mapStateToProps = state => ({
+    item: state.item
+})
+
+export default connect(
+    mapStateToProps,
+    { getItems }
+)(ShoppingList)
+
 /* In this line above < items: [...items, {id: uuid(), name }]  > the part for name is actually < name : name >
 But with destructuring, since both side is same, I am just using name
+
+connect() and mapStateToProps()
+
+To change data, we need to dispatch an action to store.
+
+On the other hand, when we want to retrieve data, we do not get it directly from the store. Instead, we get a snapshot of the data in the store at any point in time using store.getState() , which gives us the “state” of the application as on the time at which we called the getState method.
+
+This is precisely what connect does. It maps the stores state and dispatch to the props of a component :
+
+mapStateToProps and mapDispatchToProps are both pure functions that are provided the stores “state” and “dispatch” respectively. Furthermore, both functions have to return an object, whose keys will then be passed on as the props of the component they are connected to.
+
+In this case, mapStateToProps returns an object with only one key : “item”.
+
+Then the connected component (which is exported) provides item as props to ShoppingList component.
+
 */
